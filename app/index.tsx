783fed95877ece3af8b0de9c6be51c1265fc2748
tushar upadhyay <tusharupadhyay690@gmail.com>
2025-04-07 15:25:09 +0530

    Add image component and update Pressable usage in Index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text, TouchableOpacity, View } from "react-native";
+import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { style } from "../styles/authStyles";
 export default function Index() {
     return (
@@ -7,8 +7,22 @@ export default function Index() {
             <TouchableOpacity onPress={() => alert("touched")}>
                 <Text>Touch me</Text>
 
-                <Pressable></Pressable>
+                <Pressable onPress={() => alert("touched")}>
+                    <Text>Touch meee</Text>
+                </Pressable>
             </TouchableOpacity>
+
+            <Image
+                // source={require("@/assets/images/icon.png")}
+                // style={{ width: 100, height: 100 }}
+                // resizeMode="contain"
+
+                source={{
+                    uri: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffrantiger.biz%2Flisting%2Fthyrocare-franchise%2F&psig=AOvVaw2LJFNdDqYLe_X2KMQ6xs-t&ust=1744105990955000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj4-KHTxYwDFQAAAAAdAAAAABAE",
+                }}
+                style={{ width: 100, height: 100 }}
+                resizeMode="cover"
+            />
         </View>
     );
 }
